1e930f4734265ce27c62bf228db993ac6d804f90
井上雄太 <s1341011we@gmail.com>
2023-07-18 21:40:58 +0900

    [test04]

--- study/Study001.java
+++ study/Study001.java
@@ -1,5 +1,34 @@
 package study;
 
 public class Study001 {
+	
+	public static void main(String[] args) {
+		
+		int[] x = new int[20];
+		
+		for (int i = 0; i < x.length; i++) {
+            x[i] = i * 5 + 5;
+		}
+
+		for (int i = 0; i <= 10; i++) {
+            if (x[i] % 2 != 0) {
+                System.out.print(x[i]);
+                if (i != 10) {
+                    System.out.print(",");
+                }
+            }
+        }
+        System.out.println();
+        
+        for (int i = 11; i < x.length; i++) {
+            if (x[i] % 2 == 0) {
+                System.out.print(x[i]);
+                if (i != x.length - 1) {
+                    System.out.print(",");
+                }
+            }
+        }
+        System.out.println();
+	}
 
 }
